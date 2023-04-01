4eb233376ee678fc16e2e5779d90b53f6febfe8d
Rizky Tegar <55475891+rizkytegar@users.noreply.github.com>
2023-04-01 14:17:16 +0700

    update new

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,14 @@
-import NavbarComponents  from '../components/NavbarComponents'
-export default function About() {
+import NavbarComponents from '../components/NavbarComponents'
+import FooterComponents from '../components/FooterComponents'
+export default function Home() {
   return (
     <>
-    <NavbarComponents/>
+      <NavbarComponents />
+      <div className="col-lg-8 mx-auto p-4 py-md-5">
+        <h1 className="text-body-emphasis">About Page</h1>
+        <p className="fs-5 col-md-8">Quickly and easily get started with Bootstrap's compiled, production-ready files with this barebones example featuring some basic HTML and helpful links. Download all our examples to get started.</p>
+      </div>
+      <FooterComponents />
     </>
   );
 }
